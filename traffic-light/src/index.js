44e7a20c827e93bd379297de23bf913ac5e8fc71
BangGTS <bangitgts@gmail.com>
2021-04-13 00:18:25 +0700

    code file traffic

--- traffic-light/src/index.js
+++ traffic-light/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TrafficLight from './traffic/TrafficLight';
 
 ReactDOM.render(
   <React.StrictMode>
+    <TrafficLight />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
